00111705f245cd5c79b45c477ce1cf6740ba2ad3
Yurii M <ispoyar@yandex.ru>
2025-04-03 14:16:00 +0300

    modified app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import ProductList from "./products/page";
 
 export default function Home() {
-  return (
-    <>
-      <h1>Главная</h1>
-      <ProductList />
-    </>
-  );
+  return <ProductList />;
 }
