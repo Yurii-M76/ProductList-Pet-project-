bafd406c53d714a65bc81f306aee87d92ed14d77
Yurii M <ispoyar@yandex.ru>
2025-04-04 12:25:50 +0300

    delete actions slice

--- lib/features/products/actions.ts
+++
@@ -1,8 +0,0 @@
-import { fetchProducts } from "@/lib/api";
-import { TProduct } from "@/types/product.types";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-
-export const getProducts = createAsyncThunk(
-  "products/getAll",
-  async () => await fetchProducts<TProduct[]>()
-);
