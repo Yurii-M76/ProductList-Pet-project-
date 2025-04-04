e84cb76dda387445dd49732b3ce61755f952181b
Yurii M <ispoyar@yandex.ru>
2025-04-04 12:30:38 +0300

    added like handler & fixed style to like icon

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,15 +1,21 @@
-import { FC } from "react";
+"use client";
+import { FC, SyntheticEvent } from "react";
 import Image from "next/image";
+import { useDispatch, useSelector } from "@/lib/store";
+import {
+  getFavorites,
+  setFavorites,
+} from "@/lib/features/products/productsSlice";
 import { ActionIcon, LikeIcon, TrashIcon } from "../ui";
 import classes from "./product-card.module.css";
 
 type TProductCard = {
+  id: string;
   image: string;
   category: string;
   title: string;
   description: string;
   price: number;
-  isLike?: boolean;
 };
 
 const crop = (str: string, limit: number): string => {
@@ -17,12 +23,23 @@ const crop = (str: string, limit: number): string => {
 };
 
 const ProductCard: FC<TProductCard> = ({
+  id,
   image,
   category,
   title,
   description,
   price,
 }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector(getFavorites);
+  const isLike = favorites.includes(id);
+
+  const likeHandler = (evt: SyntheticEvent) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    dispatch(setFavorites(id));
+  };
+
   return (
     <div className={classes.card}>
       <div className={classes.imageWrapper}>
@@ -36,7 +53,7 @@ const ProductCard: FC<TProductCard> = ({
           loading="lazy"
         />
         <div className={classes.delete}>
-          <ActionIcon variant="outline" size="sm">
+          <ActionIcon variant="outline" size="md">
             <TrashIcon />
           </ActionIcon>
         </div>
@@ -46,9 +63,16 @@ const ProductCard: FC<TProductCard> = ({
         <div className={classes.section}>
           <div className={classes.titleWrapper}>
             <h2 className={classes.title}>{crop(title, 30)}</h2>
-            <ActionIcon variant="transparent" size="md">
-              <LikeIcon />
-            </ActionIcon>
+            <span className={classes.like}>
+              <ActionIcon
+                variant="transparent"
+                size="sm"
+                onClick={likeHandler}
+                style={isLike ? { color: "red" } : undefined}
+              >
+                <LikeIcon />
+              </ActionIcon>
+            </span>
           </div>
           <p className={classes.description}>{crop(description, 150)}</p>
         </div>
