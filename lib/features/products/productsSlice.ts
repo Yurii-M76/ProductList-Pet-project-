5b43f5d3f20725ac24829ab3faef9480540c487e
Yurii M <ispoyar@yandex.ru>
2025-04-05 22:37:13 +0300

    fixed productsSlice

--- lib/features/products/productsSlice.ts
+++ lib/features/products/productsSlice.ts
@@ -1,14 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { TProduct } from "@/types/product.types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type TInitialState = {
-  loading: boolean;
-  error?: string | null;
+  isLoading: boolean;
+  items: TProduct[];
   favorites: string[];
 };
 
 const initialState: TInitialState = {
-  loading: false,
-  error: null,
+  isLoading: true,
+  items: [],
   favorites: [],
 };
 
@@ -16,12 +17,18 @@ export const productsSlice = createSlice({
   name: "productsSlice",
   initialState,
   reducers: {
-    setFavorites: (state, action) => {
-      if (state.favorites.includes(action.payload)) {
-        state.favorites = state.favorites.filter((id) => id !== action.payload);
-      } else {
-        state.favorites = [...state.favorites, action.payload];
-      }
+    setProducts(state, action: PayloadAction<TProduct[]>) {
+      state.items = action.payload;
+      state.isLoading = false;
+    },
+    toggleFavorite(state, action: PayloadAction<string>) {
+      state.favorites = state.favorites.includes(action.payload)
+        ? state.favorites.filter((id) => id !== action.payload)
+        : [...state.favorites, action.payload];
+    },
+    deleteProduct(state, action: PayloadAction<string>) {
+      state.favorites = state.favorites.filter((id) => id !== action.payload);
+      state.items = state.items.filter((item) => item.id !== action.payload);
     },
   },
   selectors: {
@@ -29,6 +36,7 @@ export const productsSlice = createSlice({
   },
 });
 
-export const { setFavorites } = productsSlice.actions;
+export const { setProducts, toggleFavorite, deleteProduct } =
+  productsSlice.actions;
 export const { getFavorites } = productsSlice.selectors;
 export default productsSlice;
