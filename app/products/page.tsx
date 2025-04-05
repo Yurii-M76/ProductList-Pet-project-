da0428d7de6f38a5ad4d99c0bbd76af73733637b
Yurii M <ispoyar@yandex.ru>
2025-04-05 22:36:46 +0300

    refactoring ProductsPage & create Products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,44 +1,16 @@
 import { Metadata } from "next";
-import Link from "next/link";
-import { fetchProducts } from "@/lib/api";
-import ProductCard from "@/components/ProductCard/ProductCard";
-import { TProduct } from "@/types/product.types";
-import classes from "./products.module.css";
-
-const getData = async (): Promise<TProduct[]> => {
-  return await fetchProducts<TProduct[]>();
-};
+import Products from "@/components/Products/Products";
+import { getProductsFromApi } from "@/lib/features/products/products.api";
 
 export const metadata: Metadata = {
   title: "Products | SiteName",
   description: "All Products",
 };
 
-const ProductList = async () => {
-  const products = await getData();
+const ProductsPage = async () => {
+  const products = await getProductsFromApi();
 
-  const items = products.map((item) => {
-    return (
-      <Link href={`/products/${item.id}`} key={item.id}>
-        <ProductCard
-          id={item.id}
-          key={item.id}
-          category={item.category}
-          image={item.image}
-          title={item.title}
-          description={item.description}
-          price={item.price}
-        />
-      </Link>
-    );
-  });
-
-  return (
-    <>
-      <h1>Все продукты</h1>
-      <div className={classes.products}>{items}</div>
-    </>
-  );
+  return <Products products={products} />;
 };
 
-export default ProductList;
+export default ProductsPage;
---
+++ components/Products/Products.tsx
@@ -0,0 +1,83 @@
+"use client";
+import Link from "next/link";
+import { FC, SyntheticEvent, useEffect } from "react";
+import { useDispatch, useSelector } from "@/lib/store";
+import {
+  deleteProduct,
+  setProducts,
+  toggleFavorite,
+} from "@/lib/features/products/productsSlice";
+import ProductCard from "../ProductCard/ProductCard";
+import { TProduct } from "@/types/product.types";
+import classes from "./products.module.css";
+import { deleteProductFromApi } from "@/lib/features/products/products.api";
+
+type Props = {
+  products: TProduct[];
+};
+
+const Products: FC<Props> = ({ products }) => {
+  const dispatch = useDispatch();
+  const { isLoading, items, favorites } = useSelector(
+    (state) => state.productsSlice
+  );
+
+  const toggleLike = (evt: SyntheticEvent, id: string) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    dispatch(toggleFavorite(id));
+  };
+
+  const deleteHandler = async (evt: SyntheticEvent, id: string) => {
+    if (!id) {
+      console.error("ID not transmitted");
+      return;
+    }
+
+    evt.preventDefault();
+    evt.stopPropagation();
+    try {
+      await deleteProductFromApi(id);
+      dispatch(deleteProduct(id));
+    } catch (error) {
+      console.error("Failed to delete product:", error);
+    }
+  };
+
+  const productList = items.map((item) => {
+    const isLike = favorites.includes(item.id);
+
+    return (
+      <Link href={`/products/${item.id}`} key={item.id}>
+        <ProductCard
+          key={item.id}
+          product={item}
+          isLike={isLike}
+          toggleLike={toggleLike}
+          onDelete={deleteHandler}
+        />
+      </Link>
+    );
+  });
+
+  useEffect(() => {
+    if (products.length) {
+      dispatch(setProducts(products));
+    }
+  }, [dispatch, products]);
+
+  return (
+    <>
+      <h1>Все продукты</h1>
+      <div className={classes.products}>
+        {!products.length
+          ? "Products not found"
+          : isLoading
+          ? "Загрузка..."
+          : productList}
+      </div>
+    </>
+  );
+};
+
+export default Products;
